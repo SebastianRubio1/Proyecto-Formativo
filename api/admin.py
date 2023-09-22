ff0d7100718440b5e8acedf8a2298cc3b4e1219c
Sebastian Rubio <sebasrubio278@gmail.com>
2023-09-22 05:53:51 -0500

    version 1.13

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Usuario,Encargado,Estado,Cliente,Inventario,Pedido
+# Register your models here.
 
+admin.site.register(Usuario)
+admin.site.register(Encargado)
+admin.site.register(Estado)
+admin.site.register(Cliente)
+admin.site.register(Inventario)
+admin.site.register(Pedido)
 # Register your models here.
--- api/models.py
+++ api/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 
 class Usuario(models.Model):
-    correo = models.CharField(max_length=50)
-    nombre= models.CharField (max_length=30)
-    usuario= models.CharField(max_length=30)
-    password= models.CharField(max_length=20)
-    telefono= models.IntegerField()
+    correo = models.CharField(max_length=50, null=False, blank=False)
+    nombre= models.CharField (max_length=30, null=False, blank=False)
+    usuario= models.CharField(max_length=30, null=False, blank=False)
+    password= models.CharField(max_length=20, null=False, blank=False)
+    telefono= models.BigIntegerField( null=False, blank=False)
 
 
 class Encargado(models.Model):
-    usuario = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    usuario = models.CharField(max_length=20, null=False, blank=False)
+    password = models.CharField(max_length=20, null=False, blank=False)
 
 class Cliente(models.Model):
-    usuario = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    usuario = models.CharField(max_length=20, null=False, blank=False)
+    password = models.CharField(max_length=20, null=False, blank=False)
 
 class Inventario(models.Model):
-    producto= models.CharField(max_length=10)
-    cantidad= models.IntegerField()
-    reutilizable= models.CharField(max_length=10)
-    bodega= models.CharField(max_length=10)
+    producto= models.CharField(max_length=10, null=False, blank=False)
+    cantidad= models.IntegerField(null=False, blank=False)
+    reutilizable= models.CharField(max_length=10, null=False, blank=False)
+    bodega= models.CharField(max_length=10, null=False, blank=False)
 
 
 
 class Pedido(models.Model):
-    descripccion = models.CharField(max_length=200)
-    Proyecto = models.CharField(max_length=20)
-    idCliente = models.IntegerField()
-    cotizacion = models.CharField(max_length=10)
-    direccion = models.CharField(max_length=10)
+    descripccion = models.CharField(max_length=200, null=False, blank=False)
+    Proyecto = models.CharField(max_length=20, null=False, blank=False)
+    idCliente = models.IntegerField( null=False, blank=False)
+    cotizacion = models.CharField(max_length=10, null=False, blank=False)
+    direccion = models.CharField(max_length=10, null=False, blank=False)
 
 
 
